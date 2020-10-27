633d47cc235bca18e9ab0c157e97594ccec328e7
Юрий Шикин <shikin@submin.ru>
2020-10-28 00:14:14 +0400

    task2

---
+++ homeworks/lesson5/task2.py
@@ -0,0 +1,14 @@
+"""
+Создать текстовый файл (не программно), сохранить в нем несколько строк,
+выполнить подсчет количества строк, количества слов в каждой строке.
+"""
+
+filename = "source.txt"
+
+with open(filename, 'r', encoding='utf-8') as file:
+    lines = [line for line in file.readlines() if line != '\n']
+
+    print(f"В файле непустых строк:", len(lines))
+
+    for line, words_cnt in {l: len(l.split()) for l in lines}.items():
+        print(f'Строка {line[:50]}... содержит {words_cnt} слов')
